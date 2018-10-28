0e8caf5a3defacee8e5f057db3d7c4176f455d17
Cessario <43488314+Cessario@users.noreply.github.com>
2018-10-28 21:12:07 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117063/latihan58/tambahkurang/PBO210117063Latihan58TambahKurang.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117063.latihan58.tambahkurang;
+
+/**
+ *
+ * @author Deadpool
+ */
+public class PBO210117063Latihan58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JumlahBilangan jumlahbilangan = new JumlahBilangan();
+        jumlahbilangan.tampilHasilJumlah();
+        
+        SelisihBilangan selisihBilangan = new SelisihBilangan();
+        selisihBilangan.tampilSelisih();
+        
+        
+    }
+    
+}
---
+++ src/pbo2/pkg10117063/latihan58/tambahkurang/SelisihBilangan.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117063.latihan58.tambahkurang;
+
+/**
+ *
+ * @author Deadpool
+ */
+public class SelisihBilangan extends Bilangan {
+    public void tampilSelisih(){
+        System.out.println("Hasil Selisih "+this.getX()+ " - " +this.getY()+ " = " +(this.getX()-this.getY()));
+    }
+}
